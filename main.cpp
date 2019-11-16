43c1ab2ff4e9a96e9b5e5ac53d93cf9b4d4df5dd
Luis Sanchez <luis542568sanchez@gmail.com>
2019-11-16 14:35:05 -0800

    cleaning up code and adding more helper functions

--- main.cpp
+++ main.cpp
@@ -7,80 +7,118 @@ using namespace std;
 int NumberOfMessages(string filename);								//Total number of messages in session
 int NumberOfDistinctContributors(string filename);						//Total number of distinct studnets who talked
 bool DidStudentTalk(vector<string> constributors, string user);					//helper function to see if specific student has talked
+string GetStudentNameFromChatLine(string chatLine);						//helper function to return studnet name from chatline
+vector<int>NumberOfMessagesPerMinute(string filename);					
+
 int main(){
 
 	int numOfTotalMessages = 0;
 	int numOfDistinctContributors = 0;					
+
 	numOfTotalMessages = NumberOfMessages("meeting_saved_chat.txt");			//calling NumberOfMessagesfunction
 	cout<<"The total messages for this session is: "<<numOfTotalMessages<<endl;		//output of total messsages in file
+
 	numOfDistinctContributors = NumberOfDistinctContributors("meeting_saved_chat.txt");
 	cout<<"The total number of distinct contributors is: "<<numOfDistinctContributors<<endl;
+
 	return 0;
 }
 
 
 
 int NumberOfMessages(string filename){
-	int total = 0;	
-	string userMessage;
-	ifstream transcript (filename.c_str());
+	int numOfMessages = 0;	
+	string chatLine;
+	ifstream chatLog (filename.c_str());
 	
 
-	if(transcript.fail()){								
+	if(chatLog.fail()){								
 		cout<<"failed to open file\n";						//checking if file was opened correctly
-		return 0;
+		return -1;
 	}	
-	if(transcript.is_open()){							//if opened then do get line until end of
-		while(getline(transcript,userMessage)){					//file and increment counter each time
-			++total;
+
+	if(chatLog.is_open()){							        //if opened then do get line until end of
+		while(getline(chatLog,chatLine)){					//file and increment counter each time
+			++numOfMessages;
 		}
 	}
-	return (total);
+
+	return (numOfMessages);
 }
 
 
-bool DidStudentTalk(vector<string> contributors, string user){				//helper function that searches a  vector of students who have talked
+bool DidStudentTalk(vector<string> students, string student){				//helper function that searches a  vector of students who have talked
 											//and returns true if student has talked
-	for(int i = 0; i < contributors.size(); ++i){
-		if(contributors.at(i) == user){
+	for(int i = 0; i < students.size(); ++i){
+		if(students.at(i) == student){
 			return true;
 		}
 	}
+
 	return false;
 }
 
-int NumberOfDistinctContributors(string filename){
-	int numOfDistinct = 0;
-	int indexOfFrom= 0;					
-	int indexOfCol = 0;
-	string userMessage;
-	string userName;
-	vector<string> contributors;
-	ifstream transcript (filename.c_str());
+
+string GetStudentNameFromChatLine(string chatLine){					//helper function to grab student name from chat line
+	string studentName;
+	int indexOfLastLetterOfName = 0;
 	
-	if(transcript.fail()){								//checking if file was opened correctly
+	indexOfLastLetterOfName = chatLine.find(":",14) - 1;	
+	studentName.append(chatLine,14,indexOfLastLetterOfName-14);
+	
+	return(studentName);
+}
+
+
+int NumberOfDistinctContributors(string filename){
+
+	string chatLine;
+	string studentName;
+	vector<string> students;
+	ifstream chatLog (filename.c_str());
+	
+	if(chatLog.fail()){								//checking if file was opened correctly
 		cout<<"failed to open file\n";
+		return -1;
 	}
-	if(transcript.is_open()){
-		while(getline(transcript,userMessage)){					
-			userName.erase();						//clears username string at start of loop
-			indexOfFrom = userMessage.find("From");				//searching for index of from and ':' to grab the  chars in the middle 
-											//of the two (the username)
-			indexOfCol = userMessage.find(":",indexOfFrom);
-			userName.append(userMessage,indexOfFrom,indexOfCol-indexOfFrom);//appending char's in the middle of from and ';'
-			if(contributors.size() == 0){					//To always pushback first talking user
-				contributors.push_back(userName);
-				++numOfDistinct;
+	
+	if(chatLog.is_open()){
+		while(getline(chatLog,chatLine)){					
+			studentName = GetStudentNameFromChatLine(chatLine);		//helper function call to grab username from chat line	
+			if(students.size() == 0){					//To always pushback first talking user
+				students.push_back(studentName);
 			}
-			if(!DidStudentTalk(contributors,userName)){			//Using helper function to see if user has talked already
-				++numOfDistinct;					//incremeneting counter if first time user is talking
-				contributors.push_back(userName);			//pushing name into vector of students who have talked
+	
+			if(!DidStudentTalk(students,studentName)){			//Using helper function to see if user has talked before
+				students.push_back(studentName);			//pushing name into vector of students who have talk
 			}	
 		}
 
 	}
-	return numOfDistinct;
+
+	return students.size();
 }			
+
+
+/*
+vector<int>NumberOfMessagesPerMinute(string filename){
+	string currentTime;
+	string newTime;
+	vector<int>numOfMessagesInMin;
+	string userMessage;
+	ifstream(transcript (filename.c_str());
+	
+	if(transcript.fail()){
+		cout<<"\nfailed to open file\n";
+		return -1;
+	}
+
+	if(transcript.is_open()){
+		while(getline(transcript,userMessage)){
+					
+		
+*/
+
 
 
 
